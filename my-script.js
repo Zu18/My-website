dcf4ac6240a569c3cdb524b929df1eb940496c90
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-24 20:36:55 +0100

    Re-design responsive navbar by adding menu-icon and collapsed menu

--- my-script.js
+++ my-script.js
@@ -6,3 +6,25 @@ window.addEventListener("scroll", function scrollNavbar() {
         nav.classList.remove("topNavbar-scrolled");
     }
 });
+
+const menuIcon =  document.getElementById("menuIcon");
+const topNavbar = document.getElementById('myNavbar');
+const navLinks = document.querySelectorAll(".topNavbar a");
+
+menuIcon.addEventListener('click', () => {
+    console.log("you click on menuIcon")
+    topNavbar.classList.toggle('responsive');
+    menuIcon.querySelector('i').classList.toggle('fa-bars');
+    menuIcon.querySelector('i').classList.toggle('fa-times');
+});
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log("you click on a")
+        if (link.id !== "menuIcon") {
+            console.log("you click on a")
+            topNavbar.classList.remove('responsive'); // Hide the menu
+            menuIcon.querySelector('i').classList.remove('fa-times');
+            menuIcon.querySelector('i').classList.add('fa-bars');
+        }
+    });
+});
